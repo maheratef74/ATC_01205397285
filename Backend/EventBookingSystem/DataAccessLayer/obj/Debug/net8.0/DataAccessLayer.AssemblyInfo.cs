60048e6a398ea580341329647b39ae8590f61739
Maher <maheratef600@gmail.com>
2025-05-10 14:02:34 +0300

    Fix conflict

--- Backend/EventBookingSystem/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ Backend/EventBookingSystem/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dde769871e0066f11874002413624e5e867254b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396f07fe2696a7d2c7eaa7024e32d7c4ac022689")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
